adc09177d9848b38917ee1358f66ed4522e4ea40
Federico Tibaldo <fedetibaldo@protonmail.com>
2020-06-12 23:50:03 +0200

    Fix entrypoint

--- plugins/gatsby-rehype-ghost-links/index.js
+++ plugins/gatsby-rehype-ghost-links/index.js
@@ -1,1 +1,31 @@
-// noop
+const visit = require(`unist-util-visit`)
+
+exports.default = ({
+	htmlAst,
+	htmlNode,
+	reporter
+}, pluginOptions) => {
+	const {
+		cmsUrl,
+		basePath = `/`,
+	} = pluginOptions
+
+	const url = htmlNode && htmlNode.context && htmlNode.context.url
+	const slug = htmlNode && htmlNode.context && htmlNode.context.slug
+
+	if (!url && slug) {
+		reporter.warn(`Expected url and slug not defined.`)
+		return htmlAst
+	}
+
+	visit(htmlAst, { tagName: `a` }, (node) => {
+		const href = node.properties && node.properties.href
+		if (href && href.startsWith(cmsUrl)) {
+			node.properties.href = href.replace(cmsUrl, basePath)
+		} else {
+			node.properties.rel = `nofollow`
+		}
+	})
+
+	return htmlAst
+}
