b26f13a71e9a20f6927977a57cb09f7ffde764b7
Federico Tibaldo <fedetibaldo@protonmail.com>
2025-12-19 17:56:24 +0100

    fix: add plausible tracker

--- packages/website/src/settings.ts
+++ packages/website/src/settings.ts
@@ -3,6 +3,7 @@ import { z } from "astro:schema";
 const { frontmatter } = await import("./content/singletons/settings.md");
 
 const schema = z.object({
+	siteDomain: z.string(),
 	siteTitle: z.string(),
 	siteDescription: z.string(),
 });
