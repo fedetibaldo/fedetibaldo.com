41838c31f67bcd3a7b8d2f5fe1a331092b82a6ef
Federico Tibaldo <fedetibaldo@protonmail.com>
2025-12-19 18:17:44 +0100

    Revert "fix: add plausible tracker"
    This reverts commit b26f13a71e9a20f6927977a57cb09f7ffde764b7.

--- packages/website/src/settings.ts
+++ packages/website/src/settings.ts
@@ -3,7 +3,6 @@ import { z } from "astro:schema";
 const { frontmatter } = await import("./content/singletons/settings.md");
 
 const schema = z.object({
-	siteDomain: z.string(),
 	siteTitle: z.string(),
 	siteDescription: z.string(),
 });
